9bd55453485b32c3e0e122289903ae2a50141e57
Daniel Gurney <dgurney99@gmail.com>
2021-02-14 20:09:57 +0200

    Support Windows 95 mode

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dgurney/unikey/validator"
 )
 
-const version = "0.3.5"
+const version = "0.3.6"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -38,7 +38,8 @@ func main() {
 	repeat := flag.Int("r", 1, "Generate n keys.")
 	t := flag.Bool("t", false, "Show how long the generation took.")
 	validate := flag.String("v", "", "Validate a CD or OEM key.")
-	ver := flag.Bool("ver", false, "Show version information and exit")
+	ver := flag.Bool("ver", false, "Show version information and exit.")
+	Is95 := flag.Bool("95", false, "Apply Windows 95 rules to OEM/CD key validation.")
 	flag.Parse()
 
 	if *ver {
@@ -76,6 +77,7 @@ func main() {
 			ki = validator.Mod7CD{
 				First:  k[0:3],
 				Second: k[4:11],
+				Is95:   *Is95,
 			}
 		case len(k) == 23 && k[5:6] == "-" && k[9:10] == "-" && k[17:18] == "-" && len(k[18:]) == 5:
 			ki = validator.Mod7OEM{
@@ -84,6 +86,7 @@ func main() {
 				Second: k[6:9],
 				Third:  k[10:17],
 				Fourth: k[18:],
+				Is95:   *Is95,
 			}
 		default:
 			fmt.Println("Could not recognize key type")
