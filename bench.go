26470fa93e00ecce8804771e8f1008a6e3a30a34
Daniel Gurney <dgurney99@gmail.com>
2021-04-17 18:25:39 +0300

    2021

--- bench.go
+++ bench.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*
-   Copyright (C) 2020 Daniel Gurney
+   Copyright (C) 2021 Daniel Gurney
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
